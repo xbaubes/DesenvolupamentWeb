f0d023993a7db604f6d8e6a557f2d7217fbf0ab4
Xavier Baubés Parramon <xavierbaubes@gmail.com>
2022-03-01 16:19:22 +0100

    MapaTresor

---
+++ MapaTresor.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Introdueix amplada mapa:");
+int amplada;
+while(!Int32.TryParse(Console.ReadLine(), out amplada));
+Console.WriteLine("Introdueix alçada mapa:");
+int alcada;
+while (!Int32.TryParse(Console.ReadLine(), out alcada)) ;
+
+int nombreX;
+int nombreY;
+do
+{
+    Console.WriteLine("Introdueix posicio x de la marca:");
+    nombreX = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Introdueix posicio y de la marca:");
+    nombreY = Convert.ToInt32(Console.ReadLine());
+} while (amplada < nombreX || alcada < nombreY);
+
+string cadena = "";
+
+for (int y = 0; y <= alcada + 1; y++)
+{
+    for (int x = 0; x <= amplada + 1; x++)
+    {
+        if (x == 0 || y == 0 || x == amplada + 1 || y == alcada + 1)
+        {
+            cadena += "*";
+        }
+        else if (x == nombreX && y == nombreY)
+        {
+            cadena += "X";
+        }
+        else
+        {
+            cadena += "·";
+        }
+    }
+    Console.WriteLine(cadena);
+    cadena = "";
+}
