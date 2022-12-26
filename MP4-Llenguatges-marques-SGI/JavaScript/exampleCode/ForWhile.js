964ff9792b52c4be3d9a89d2b49ea35eff6960b7
Baubés Parramon <xavierbaubes@gmail.com>
2022-12-26 12:23:33 +0100

    do ... while loop added

--- MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/ForWhile.js
+++ MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/ForWhile.js
@@ -2,7 +2,7 @@
 
 for (let i = 0; i < 5; i++)
 {
-    console.log("iteracio n: " + i);
+    console.log("iteracio for n: " + i);
 }
 
 /* WHILE */
@@ -10,6 +10,14 @@ for (let i = 0; i < 5; i++)
 let i = 0;
 while (i < 5)
 {
-    console.log("iteracio n: " + i);
+    console.log("iteracio while n: " + i);
     i++;
 }
+
+/* DO ... WHILE */
+
+let ii = 0;
+do {
+    console.log("iteracio do...while n: " + ii);
+    ii++;
+} while(ii > 100);
