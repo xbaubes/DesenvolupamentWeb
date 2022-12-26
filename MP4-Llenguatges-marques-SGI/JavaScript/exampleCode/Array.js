c555f8f37d646cac06b73365f23c21b15fbf081c
Baubés Parramon <xavierbaubes@gmail.com>
2022-12-26 12:25:28 +0100

    basic second level concepts (arrays & functions)

---
+++ MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/Array.js
@@ -0,0 +1,41 @@
+const cars = new Array("Saab", "Volvo", "BMW"); //new Array()
+
+const fullCars = ["Saab", "Volvo", "BMW"]; //[]
+
+const emptyCars = [];
+emptyCars[0]= "Saab";
+emptyCars[1]= "Volvo";
+emptyCars[emptyCars.length]= "BMW"; // afegit a la primera posicio buida
+
+console.log(fullCars, emptyCars);
+console.log(fullCars[1]);
+console.log(emptyCars[emptyCars.length -1]); //obtenim l ultim element
+
+/*******/
+
+let sum = 0;
+const numbers = [5, 5, 5];
+
+numbers.forEach(mySumFunction);
+function mySumFunction(item) { //la funcio esta en el mateix ambit que l array
+    sum += item;
+}
+console.log(sum);
+
+/*******/
+
+const myNumbers = [45, 4, 9, 16, 25];
+
+const numbers2 = myNumbers.map(myMultiFunction);
+function myMultiFunction(value, index, array) { // els parametres es passen automaticament i son opcionals
+    console.log(index);
+    console.log(array);
+    return value * 2; // multiplica per dos cada element de l array
+}
+console.log(numbers2);
+
+/*******/
+
+const myGirls = ["Cecilie", "Lone"];
+const over4 = myGirls.filter(myGirl => myGirl.length > 4); // amb arrow function
+console.log(over4);
---
+++ MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/Function.js
@@ -0,0 +1,22 @@
+let valor1 = 5, valor2 = 4;
+
+function multiplicacio1 (v1, v2) {
+    return v1 * v2;
+}
+let resultat1 = multiplicacio1(valor1,valor2);
+
+/* declaracio alternativa amb anonymous function */
+
+let multiplicacio2 = (function (v1,v2) {
+    return v1 * v2;
+});
+let resultat2 = multiplicacio2(valor1,valor2);
+
+/* declaracio alternativa amb arrow function */
+
+let multiplicacio3 = (v1,v2) => { return v1 * v2 };
+let resultat3 = multiplicacio3(valor1,valor2);
+
+/* resultats equivalents */
+
+console.log("Resultat de la multiplicacio:" , resultat1 , resultat2, resultat3);
