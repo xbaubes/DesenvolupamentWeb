75ff5edbfdaabd75a65dd39f140c40a61f301bf6
Baubés Parramon <xavierbaubes@gmail.com>
2022-12-26 16:44:31 +0100

    nested loop example

--- MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/ForWhile.js
+++ MP4-Llenguatges-marques-SGI/JavaScript/exampleCode/ForWhile.js
@@ -21,3 +21,16 @@ do {
     console.log("iteracio do...while n: " + ii);
     ii++;
 } while(ii > 100);
+
+/* NESTED LOOP */
+
+console.log("nested loop");
+for (let i = 0; i < 5; i++)
+{
+    console.log("iteracio OUTSIDE: [" + i + "]");
+    for (let i2 = 0; i2 < 5; i2++)
+    {
+        console.log("iteracio INSIDE: " + i2);
+    }
+    console.log("-");
+}
