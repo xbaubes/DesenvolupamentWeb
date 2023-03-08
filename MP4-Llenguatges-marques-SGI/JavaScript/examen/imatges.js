1b5fe65944bd8828b86562bf58a90a6598613a60
Baubés Parramon <xavierbaubes@gmail.com>
2023-03-08 14:25:46 +0100

    JS complete exercise DOM+Events

---
+++ MP4-Llenguatges-marques-SGI/JavaScript/examen/imatges.js
@@ -0,0 +1,33 @@
+/**
+ * @author Xavier Baubes Parramon <xbaubes@xtec.cat> feat. ChatGPT
+**/
+
+// Obtenim les miniatures
+var miniatures = document.querySelectorAll("#galeria img");
+
+// Obtenim l'element on es mostrarà la imatge gran
+var imatgeGran = document.getElementById("imatge-gran");
+    //forma alternativa:
+    //var imatgeGran = document.querySelector("#imatge-gran");
+
+// Afegim un esdeveniment de clic a cada miniature
+for (let miniatura of miniatures)
+{
+    miniatura.addEventListener("click", function(e) {
+        // Obtenim la ruta de la imatge gran a traves del dataset 'data-gran'
+        console.log(e)
+        let rutaGran = e.target.dataset.gran;
+            //formes alternatives:
+            //let rutaGran = this.dataset.gran;
+            //let rutaGran = miniatura.dataset.gran;
+
+        // Mostrar la imatge gran
+        imatgeGran.innerHTML = "<img src='" + rutaGran + "' alt='Imatge gran'>";
+            //forma alternativa (createElement):
+            //imatgeGran.innerHTML = "";
+            //let imatge = document.createElement("img"); // Creem element image
+            //imatge.src = rutaGran;
+            //imatge.alt = "Imatge gran";
+            //imatgeGran.appendChild(imatge); // Afegim l element imatge a l element 'imatge-gran'
+    });
+}
