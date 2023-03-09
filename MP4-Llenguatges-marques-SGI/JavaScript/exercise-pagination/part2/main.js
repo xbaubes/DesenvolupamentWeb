446d7112c0f9ea99859e8218a90a2b1dce1a7a8d
Baubés Parramon <xavierbaubes@gmail.com>
2023-03-09 23:00:52 +0100

    fixed code on csv.js [processCSV: added params hiddenDiv and useIndex - createAllDivInfo: added param classes]

--- MP4-Llenguatges-marques-SGI/JavaScript/exercise-pagination/part2/main.js
+++ MP4-Llenguatges-marques-SGI/JavaScript/exercise-pagination/part2/main.js
@@ -5,13 +5,13 @@ import { processCSV } from "./csv.js";
 const linesByPage = 10;
 const hiddenDiv = "infoOcult";
 const useIndex = true;
-const colSeparator = /* WORK IN PROGRESS ... */
-const rowSeparator = /* WORK IN PROGRESS ... */
+const colSeparator = /* TO DO */
+const rowSeparator = /* TO DO */
 
 // HTML ELEMENTS
 const csvForm = document.getElementById("csvForm");
-const pagesContainer = /* select #ContenidorInfo */ /* WORK IN PROGRESS ... */
-const paginatorList = /* select #llistaPagines */ /* WORK IN PROGRESS ... */
+const pagesContainer = /* select #ContenidorInfo */ /* TO DO */
+const paginatorList = /* select #llistaPagines */ /* TO DO */
 
 // PROGRAM STARTS READING CSV
 csvForm.addEventListener("submit", function (event) {
@@ -19,7 +19,7 @@ csvForm.addEventListener("submit", function (event) {
     const reader = new FileReader();
     reader.readAsText(event.target.elements.csvFile.files[0]);
     reader.onloadend = () => {
-        let divs = processCSV(pagesContainer, reader.result, linesByPage, colSeparator, rowSeparator);
+        let divs = processCSV(pagesContainer, reader.result, hiddenDiv, linesByPage, useIndex, colSeparator, rowSeparator);
         createPagination(paginatorList, divs.length, divs, hiddenDiv);
     };
 });
