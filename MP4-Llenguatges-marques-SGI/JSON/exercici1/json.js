be62aff6f733f725eaa57a7fb95f1eef8adee88d
Baubés Parramon <xavierbaubes@gmail.com>
2023-03-22 22:46:56 +0100

    read JSON directly

--- MP4-Llenguatges-marques-SGI/JSON/exercici1/json.js
+++ MP4-Llenguatges-marques-SGI/JSON/exercici1/json.js
@@ -1,4 +1,14 @@
-console.log(typeof cotxe);
-console.log(cotxe);
+var xhttp = new XMLHttpRequest();
 
-/** TO DO **/
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200)
+	{
+		console.log(typeof this.responseText);
+		console.log(this.responseText);
+		
+		/** TO DO **/
+    }
+};
+
+xhttp.open("GET", "cotxes.json", true);
+xhttp.send();
