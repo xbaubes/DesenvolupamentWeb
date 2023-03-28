ac7f8d9eac01ca1f5259edcc6eb0d6419643ded7
Baubés Parramon <xavierbaubes@gmail.com>
2023-03-28 10:26:24 +0200

    completed exercise

--- MP4-Llenguatges-marques-SGI/TypeScript/exercise1/JsToTs.js
+++ MP4-Llenguatges-marques-SGI/TypeScript/exercise1/JsToTs.js
@@ -1,6 +1,10 @@
+num1 = 5;
+let num2 = 10;
+num2 = '10';
+
+const result = multiply(num1, num2);
+console.log(result);
+
 function multiply(a, b) {
   return a * b;
 }
-
-const result = multiply(5, '10');
-console.log(result);
