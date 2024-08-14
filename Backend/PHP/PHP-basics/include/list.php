d8fd317f97d3ccc8e398e1e8fc5e023bda3ecbfd
XBP <xbaubes@xtec.cat>
2024-08-14 14:55:51 +0200

    PHP include list

---
+++ Backend/PHP/PHP-basics/include/list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables PHP llistades amb include</title>
+</head>
+<body>
+
+    <?php include 'variables.php'; ?>
+
+</body>
+</html>
---
+++ Backend/PHP/PHP-basics/include/variables.php
@@ -0,0 +1,13 @@
+<?php
+        // Declarem les variables
+        $variable1 = "Primera variable";
+        $variable2 = "Segona variable";
+        $variable3 = "Tercera variable";
+        
+        // Generem la llista HTML
+        echo "<ul>";
+        echo "<li>$variable1</li>";
+        echo "<li>$variable2</li>";
+        echo "<li>$variable3</li>";
+        echo "</ul>";
+?>
