b68ae45c20ccd51b46eae59908b5ebe3f78c9d6b
XBP <xbaubes@xtec.cat>
2024-08-16 21:08:00 +0200

    PHP SESSION

---
+++ Backend/PHP/PHP-dades/session/sessionCreation.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Sessió Mostrar Variables</title>
+</head>
+<body>
+
+    <?php
+        session_start();
+
+        // Assignar valors a la sessió
+        if (!isset($_SESSION['usuari']) && !isset($_SESSION['rol']))
+        {
+            echo "<p>CREACIO DADES SESSION</p>";
+            $_SESSION['usuari'] = 'Joan';
+            echo "<p>Usuari assignat</p>";
+            $_SESSION['rol'] = 'admin';
+            echo "<p>Rol assignat</p>";
+        }
+    ?>
+
+    <a href="sessionUnset.php">
+        <button>Testing session : Show values</button>
+    </a>
+
+</body>
+</html>
---
+++ Backend/PHP/PHP-dades/session/sessionUnset.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Sessió Assignar Valor Variables</title>
+</head>
+<body>
+
+    <?php
+        session_start();
+        
+        // Intentar accedir a les variables de sessió
+        if (isset($_SESSION['usuari']) && isset($_SESSION['rol']))
+        {
+            echo "<p>ACCES A DADES SESSION</p>";
+            echo "<p>Usuari: " . $_SESSION['usuari'] . "</p><p>Rol: " . $_SESSION['rol'] . "</p>"; 
+        }
+
+        // Buidar totes les variables de sessió
+        session_unset();
+    ?>
+
+    <a href="sessionCreation.php">
+        <button>Testing session : Setting values</button>
+    </a>
+
+</body>
+</html>
