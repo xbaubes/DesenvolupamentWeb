781034f12c601c9df5fb5ae82eb47b43439a9528
XBP <xbaubes@xtec.cat>
2024-08-18 00:06:47 +0200

    using cookie example

---
+++ Backend/PHP/PHP-dades/cookie.php
@@ -0,0 +1,42 @@
+<?php
+
+    // Llegir una cookie
+    if (!isset($_COOKIE['nom_usuari'])) {
+        // Creació d'una cookie
+        // Estableix una cookie que expira en 30 dies
+        setcookie("nom_usuari", "Joan", time() + (30 * 24 * 60 * 60), "/");
+        //30 dies * 24 hores que té un dia * 60 minuts que té una hora * 60 segons que té un minut
+
+        // La cookie no estarà disponible fins la pròxima sol.licitud HTTP
+        if (isset($_COOKIE['nom_usuari'])) {
+            echo "La cookie s'ha creat correctament.<br> El valor és: " . $_COOKIE['nom_usuari'] . "<br>";
+        } else {
+            echo "La cookie encara no està disponible.";
+        }
+    } else {
+        echo "La cookie ja estava creada.<br> El valor és: " . $_COOKIE['nom_usuari'] . "<br>";
+    }
+
+    // Eliminació d'una cookie
+    if (isset($_POST['eliminar_cookie'])) {
+        // Per eliminar una cookie, cal establir-la amb un temps passat
+        setcookie("nom_usuari", "", time() - 3600, "/");
+        echo "La cookie s'ha eliminat.<br>";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--Formulari per eliminar la cookie -->
+    <form method="post" action="">
+        <input type="submit" name="eliminar_cookie" value="Eliminar Cookie">
+    </form>
+</body>
+</html>
