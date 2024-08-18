74a746ba9accb7cf3ed93b6564d2671625ca2051
XBP <xbaubes@xtec.cat>
2024-08-18 12:50:03 +0200

    cookie activity to resolve

---
+++ Backend/PHP/PHP-dades/cookieActivitat/index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pàgina amb preferències de tema</title>
+    <link rel="stylesheet" href="tema.css">
+</head>
+<body class="tema-clar">
+
+    <h1>Configura el Tema de la Pàgina</h1>
+    <form>
+        <label>
+            <input type="radio" name="tema" value="clar">
+            Tema clar
+        </label>
+        <label>
+            <input type="radio" name="tema" value="fosc">
+            Tema fosc
+        </label>
+        <br>
+        <button type="submit">Guardar Preferències</button>
+    </form>
+
+    <p>Actualment estàs utilitzant el tema <strong>clar</strong>.</p>
+
+</body>
+</html>
