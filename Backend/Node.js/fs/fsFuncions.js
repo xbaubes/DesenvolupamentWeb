13342025e1868a71b3beb869387f940b7bdb6f66
XBP <xbaubes@xtec.cat>
2024-09-12 10:57:14 +0200

    fs with callbacks

--- Backend/Node.js/fs/fsFuncions.js
+++ Backend/Node.js/fs/fsFuncions.js
@@ -1,37 +1,57 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 
 // Ruta del fitxer
 const filePath = './example.txt';
 
-async function manageFileOperations() {
-    try {
-        // Crear un fitxer amb fs.appendFile
-        await fs.appendFile(filePath, 'Hello content!\n');
+function manageFileOperations() {
+    // Crear un fitxer amb fs.appendFile
+    fs.appendFile(filePath, 'Hello content!\n', (err) => {
+        if (err) {
+            return console.error('Error appending file:', err);
+        }
         console.log('File created and initial content added!\n');
 
         // Llegir el fitxer amb fs.readFile
-        let data = await fs.readFile(filePath, 'utf8');
-        console.log('File content after initial write:\n', data);
+        fs.readFile(filePath, 'utf8', (err, data) => { // Patró error-first: el primer paràmetre del callback és per controlar l'error, el segon és el retorn de la funció
+            if (err) {
+                return console.error('Error reading file:', err);
+            }
+            console.log('File content after initial write:\n', data);
 
-        // Actualitzar el fitxer amb fs.appendFile
-        await fs.appendFile(filePath, 'This is additional content.\n');
-        console.log('Content added to the file!\n');
+            // Actualitzar el fitxer amb fs.appendFile
+            fs.appendFile(filePath, 'This is additional content.\n', (err) => {
+                if (err) {
+                    return console.error('Error appending file:', err);
+                }
+                console.log('Content added to the file!\n');
 
-        // Llegir el fitxer amb fs.readFile per veure les actualitzacions
-        data = await fs.readFile(filePath, 'utf8');
-        console.log('File content after update:\n', data);
+                // Llegir el fitxer amb fs.readFile per veure les actualitzacions
+                fs.readFile(filePath, 'utf8', (err, data) => {
+                    if (err) {
+                        return console.error('Error reading file:', err);
+                    }
+                    console.log('File content after update:\n', data);
 
-        // Canviar el nom del fitxer amb fs.rename
-        const newFilePath = './renamed_example.txt';
-        await fs.rename(filePath, newFilePath);
-        console.log('File renamed to "renamed_example.txt"!\n');
+                    // Canviar el nom del fitxer amb fs.rename
+                    const newFilePath = './renamed_example.txt';
+                    fs.rename(filePath, newFilePath, (err) => {
+                        if (err) {
+                            return console.error('Error renaming file:', err);
+                        }
+                        console.log('File renamed to "renamed_example.txt"!\n');
 
-        // Eliminar el fitxer amb fs.unlink
-        await fs.unlink(newFilePath);
-        console.log('File deleted!\n');
-    } catch (err) {
-        console.error('Error:', err);
-    }
+                        // Eliminar el fitxer amb fs.unlink
+                        fs.unlink(newFilePath, (err) => {
+                            if (err) {
+                                return console.error('Error deleting file:', err);
+                            }
+                            console.log('File deleted!\n');
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
 
 // Executa la funció
