b4593ba9704e597c2e6e7d8cb68e9318e9cf42dd
XBP <xbaubes@xtec.cat>
2024-09-21 23:33:32 +0200

    exercises about arrays

--- Frontend/JavaScript/exampleCode/Array.js
+++ Frontend/JavaScript/exampleCode/Array.js
@@ -9,11 +9,11 @@ emptyCars[0]= "Saab";
 emptyCars[1]= "Volvo";
 emptyCars[emptyCars.length]= "BMW"; // afegit a la primera posicio buida
 
-console.log(fullCars, emptyCars);
+console.log(cars, fullCars, emptyCars);
 console.log(fullCars[1]);
 console.log(emptyCars[emptyCars.length -1]); //obtenim l ultim element
 
-/*******/
+console.log("/********************************************************/");
 
 let sum = 0;
 const numbers = [5, 5, 5];
@@ -24,19 +24,18 @@ function mySumFunction(item) { //la funcio esta en el mateix ambit que l array
 }
 console.log(sum);
 
-/*******/
+console.log("/********************************************************/");
 
 const myNumbers = [45, 4, 9, 16, 25];
 
 const numbers2 = myNumbers.map(myMultiFunction);
 function myMultiFunction(value, index, array) { // els parametres es passen automaticament i son opcionals
-    console.log(index);
-    console.log(array);
+    console.log(index, array);
     return value * 2; // multiplica per dos cada element de l array
 }
 console.log(numbers2);
 
-/*******/
+console.log("/********************************************************/");
 
 const myGirls = ["Cecilie", "Lone"];
 const over4 = myGirls.filter(myGirl => myGirl.length > 4); // amb arrow function
